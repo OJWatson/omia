08f27f2ce0793e4be6075c617e18e59f6895e287
Oliver Watson <o.watson15@imperial.ac.uk>
2026-03-03 11:24:37 +0000

    Exclude local R check artifacts and source tarball from version control

--- ..Rcheck/tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(omia)
-
-test_check("omia")
--- omia.Rcheck/00_pkg_src/omia/tests/testthat/test-dataset.R
+++
@@ -1,7 +0,0 @@
-test_that("cat_ba_week1 dataset is available and valid", {
-  data("cat_ba_week1", package = "omia")
-
-  expect_true(exists("cat_ba_week1"))
-  expect_equal(nrow(cat_ba_week1), 23)
-  expect_equal(sum(cat_ba_week1$incidence), 79)
-})
